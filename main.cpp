3225371a5877a65c6baf242a665b27f55c0f493a
halkaabi3 <halkaabi3@mail.csuchico.edu>
2018-02-23 19:04:56 -0800

    Grocery

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//Author: Helal Alkaabi
 #include<iostream>
 #include<string>
 using namespace std;
@@ -5,15 +6,34 @@ using namespace std;
 int main()
 {
   string list[5]; //array of 5 strings
-  int numItems = 0;
-  char input;
+    int numItems = 0;
+      char input;
+        string input2;
+         do
+           {
 
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
+             cout<<"\n==GROCERY LIST MANAGER==";
+               cout<<"\nEnter your choice: ";
+                 cout<<"\n (A)dd an item";
+                   cout<<"\n (Q)uit";
+                     cout<<"\nYour choice (A/Q): ";
+                       cin>>input;
+                         if(input == 'A' || input == 'a')
+                           {
+                                  cout<<"What is the item?\n";
+                                         cin>>input2;
+                                           if(numItems<5)
+                                             {
+                                                    list[numItems]=input2;
+                                                           numItems++;
+                                                             }
+                                                               else
+                                                                 {
+                                                                        cout<<"You'll need a bigger list!\n";
+                                                                          }
+                                                                            }
+                                                                            }  while(input!='q' && input!='Q');
 
-  return 0;
-}
+                                                                              return 0;
+                                                                              }
+
